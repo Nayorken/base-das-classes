f2737fec03f1ba10c05863c06e716f885ecd32ad
hugo ferreira <hugoytbrg.2002@gmail.com>
2019-11-18 15:56:26 +0000

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criclass
+{
+    class Class1
+    {
+        private double valor;
+        public string recebe
+        {
+            set
+            {
+                valor = Convert.ToDouble(value);
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                return(valor + 2).ToString();
+            }
+        }
+    }
+}
